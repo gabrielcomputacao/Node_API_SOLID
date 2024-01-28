24f7b74bbf69e88c907efa379d203e0a4c5b65a2
gabriel <gabrielcomputacao@outlook.com>
2024-01-28 09:55:06 -0300

    feat: configuring project

---
+++ src/env/index.ts
@@ -0,0 +1,23 @@
+import { config } from "dotenv";
+
+import { z } from "zod";
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
+  /* coerce do zod converte o valor para o proximo valor definido depois dele */
+  PORT: z.coerce.number().default(3333),
+});
+
+//  todo: safeParse tenta verificar o que foi definido a cima se todas as variaveis tem o mesmo tipo
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  /* metodo format pega todos os erros que aconteceram e formata eles de uma maneira mais amigavel */
+  console.error("invalid enviroment variables", _env.error.format());
+
+  // colocando o throw em top level sem ser dentro de uma função, ele nao deixa o codigo passar, ele derruba a aplicação
+  throw new Error("invalid enviroment variables");
+}
+
+export const env = _env.data;

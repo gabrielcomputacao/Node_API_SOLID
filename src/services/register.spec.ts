623a5ea1b205f73ffcd264268843baba0535ee1b
gabriel <gabrielcomputacao@outlook.com>
2024-02-02 06:55:19 -0300

    feat:  created unit tests int the services file

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -2,31 +2,33 @@ import { expect, describe, it } from "vitest";
 import { RegisterService } from "./register";
 
 import { compare } from "bcryptjs";
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 
 // ! teste unitario nunca vai tocar em banco de dados nem em camadas internas, quando chama ou mexe com o banco ou dados externos e teste de integração ou outros
 
 describe("Register Services", () => {
+  it("should be able to register", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const registerService = new RegisterService(usersRepository);
+
+    const { user } = await registerService.execute({
+      name: "John Doe",
+      email: "john@hotmail.com",
+      password: "1234567",
+    });
+
+    // espera que o id do usuario seja igual a qualquer string , nesse caso para verificar se o usuario foi criado de fato
+    expect(user.id).toEqual(expect.any(String));
+  });
   it("should hash user password upon registration", async () => {
     /* 
         Enviando um objeto ficticio nao bate no banco de dados, e por isso e uma boa criar um repositorio fake para fazer
         os testes, para nao sujar o banco nem demorar os testes.
     */
 
-    const registerService = new RegisterService({
-      async findByEmail(email: any) {
-        return null;
-      },
-
-      async create(data: any) {
-        return {
-          id: "user-1",
-          name: data.name,
-          email: data.email,
-          password_hash: data.password_hash,
-          created_at: new Date(),
-        };
-      },
-    });
+    const usersRepository = new InMemoryUsersRepository();
+    const registerService = new RegisterService(usersRepository);
 
     const { user } = await registerService.execute({
       name: "John Doe",
@@ -42,5 +44,27 @@ describe("Register Services", () => {
     );
 
     expect(isPasswordCorrectlyHashed).toBe(true);
+  });
+  it("should not be able to register with same email twice", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const registerService = new RegisterService(usersRepository);
+
+    const email = "john@hotmail.com";
+
+    await registerService.execute({
+      name: "John Doe",
+      email,
+      password: "1234567",
+    });
+
+    // todo: Sempre que tiver um promise dentro do expect adicionar o await
+
+    await expect(() =>
+      registerService.execute({
+        name: "John Doe",
+        email,
+        password: "1234567",
+      })
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError);
   });
 });

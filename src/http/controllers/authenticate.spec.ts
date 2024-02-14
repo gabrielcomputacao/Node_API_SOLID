f0419a23ecfe288359d660fbad30926950ac079a
gabriel <gabrielcomputacao@outlook.com>
2024-02-14 07:12:15 -0300

    feat: creating tests e2e for register and authenticate

---
+++ src/http/controllers/authenticate.spec.ts
@@ -0,0 +1,29 @@
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { app } from "@/app";
+import request from "supertest";
+
+describe("authenticate (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to authenticate"),
+    async () => {
+      await request(app.server).post("/users").send({
+        name: "John Doe",
+        email: "Johndoe@example.com",
+        password: "1234567",
+      });
+      const response = await request(app.server).post("/sessions").send({
+        email: "Johndoe@example.com",
+        password: "1234567",
+      });
+      expect(response.statusCode).toEqual(200);
+      expect(response.body).toEqual({
+        token: expect.any(String),
+      });
+    };
+});
